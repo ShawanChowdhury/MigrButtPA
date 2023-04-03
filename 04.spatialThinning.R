2cd7a750662bba3228bb89cc2bb0bfee0489a14b
ShawanChowdhury <s.chowdhury@uqconnect.edu.au>
2023-04-03 23:50:18 +1000

    Updating scripts
    Spatial thinning without spThin, New rules for SDMs

--- 04.spatialThinning.R
+++ 04.spatialThinning.R
@@ -1,56 +1,59 @@
-# Loading required libraries
-library(raster)
-library(dismo)
-library(spThin)
-library(tidyverse)
-library(dplyr)
+# The spatial thinning process using the 'spThin' process takes a long time, so we thinned the spatial
+# data when developing the model using the ENMeval package.
 
-data <- read_csv("data/cleanedRecords_gbif.csv")
-head(data)
-
-season <- unique(data$season)
-
-# Test-run
-# g <- "Sino-Japanese"
-# h <- "S4"
-# i <- "Aporia crataegi"
-
-for (h in season) {
-    
-    print(h)
-    
-    ses_data <- data %>% 
-      filter(season == h)
-    
-    species <- unique(ses_data$species)
-    
-    for (i in species){
-      print(i)
-      
-      sp_data <- ses_data %>% 
-        filter(species == i)
-      
-      # Spatial thinning
-      thinned_dataset_full <-
-        thin( loc.data = sp_data, 
-              lat.col = "decimalLatitude", long.col = "decimalLongitude", 
-              spec.col = "species", 
-              thin.par = 4.65, reps = 10000, 
-              locs.thinned.list.return = TRUE, 
-              write.files = FALSE, 
-              write.log.file = FALSE)
-      
-      max_idx <- which.max(sapply(thinned_dataset_full, nrow))
-      thinned_dataset_max_rows <- thinned_dataset_full [[max_idx]]
-      colnames(thinned_dataset_max_rows) <- c("decimalLongitude", "decimalLatitude")
-      
-      thin_data <- thinned_dataset_max_rows %>%
-        dplyr::select("decimalLongitude", "decimalLatitude") %>% 
-        dplyr::mutate(species = i)
-      
-      write.csv(thin_data, paste0("data/thinned/", h, i, "_thin.csv"), row.names = FALSE)
-    }
-    
-}
-
-
+# # Loading required libraries
+# library(raster)
+# library(dismo)
+# library(spThin)
+# library(tidyverse)
+# library(dplyr)
+# 
+# data <- read_csv("data/cleanedRecords_gbif.csv")
+# head(data)
+# 
+# season <- unique(data$season)
+# 
+# # Test-run
+# # g <- "Sino-Japanese"
+# # h <- "S4"
+# # i <- "Aporia crataegi"
+# 
+# for (h in season) {
+#     
+#     print(h)
+#     
+#     ses_data <- data %>% 
+#       filter(season == h)
+#     
+#     species <- unique(ses_data$species)
+#     
+#     for (i in species){
+#       print(i)
+#       
+#       sp_data <- ses_data %>% 
+#         filter(species == i)
+#       
+#       # Spatial thinning
+#       thinned_dataset_full <-
+#         thin( loc.data = sp_data, 
+#               lat.col = "decimalLatitude", long.col = "decimalLongitude", 
+#               spec.col = "species", 
+#               thin.par = 4.65, reps = 10000, 
+#               locs.thinned.list.return = TRUE, 
+#               write.files = FALSE, 
+#               write.log.file = FALSE)
+#       
+#       max_idx <- which.max(sapply(thinned_dataset_full, nrow))
+#       thinned_dataset_max_rows <- thinned_dataset_full [[max_idx]]
+#       colnames(thinned_dataset_max_rows) <- c("decimalLongitude", "decimalLatitude")
+#       
+#       thin_data <- thinned_dataset_max_rows %>%
+#         dplyr::select("decimalLongitude", "decimalLatitude") %>% 
+#         dplyr::mutate(species = i)
+#       
+#       write.csv(thin_data, paste0("data/thinned/", h, i, "_thin.csv"), row.names = FALSE)
+#     }
+#     
+# }
+# 
+# 
--- 05.suitabilityMaps.R
+++ 05.suitabilityMaps.R
@@ -12,79 +12,124 @@ library(data.table)
 library(stringr)
 library(rgdal)
 library(data.table)
-
-# This script is for S1 Palaearctic, we repeated the same procedure for 11 realms and four seasons (plus year-round models).
+library(tidyverse)
 
 # Reading csv file
-dc1 <- read.csv("S1.Palaearctic.csv", header = T)
+data <- read_csv("data/cleanedRecords_gbif.csv", header = T)
 
-dc_cl <- dc1 %>%
-  dplyr::select("species", "decimalLon", "decimalLat")
+data <- data %>%
+  dplyr::select("species", "decimalLongitude", "decimalLatitude", "season")
 
 
 # Remove records without coordinates
-dc_cl <- dc_cl%>%
-  filter(!is.na(decimalLon))%>%
-  filter(!is.na(decimalLat))%>%
-  filter(!is.na(species))
+data <- data %>%
+  filter(!is.na(species))%>%
+  filter(!is.na(decimalLongitude))%>%
+  filter(!is.na(decimalLatitude))%>%
+  filter(!is.na(season))
 
 # Remove species with low occurrence records
-dc_cl <- dc_cl %>%
+data_cl <- data %>%
   group_by(species) %>%
-  filter(n() > 3) %>%
+  filter(n() > 29) %>%
   ungroup()
 
 # Remove duplicated records
-dc_cl <- dc_cl[!duplicated(dc_cl),]
+data_cl <- data_cl[!duplicated(data_cl),]
 
 #attaching climatic layers
-vars<-c("bio1.tif","bio2.tif","bio3.tif","bio4.tif","bio5.tif","bio6.tif","bio7.tif","bio8.tif","bio9.tif","bio10.tif","bio11.tif","bio12.tif","bio13.tif","bio14.tif","bio15.tif","bio16.tif","bio17.tif","bio18.tif","bio19.tif")
-clim.stack <- stack(paste(getwd(),"/clim/", vars, sep=""))
+vars<-c("bio1.tif","bio2.tif","bio3.tif","bio7.tif","bio15.tif","bio17.tif","bio18.tif","bio19.tif")
+envs <- stack(paste(getwd(),"/data/clim/", vars, sep=""))
 
-species <- unique(dc_cl$species)
+season <- unique(data_cl$season)
 
-for (currspecies in species) try({
-  print(currspecies)
-  speciesname <- gsub(" ", "_", currspecies)
-  dc <- dc_cl %>% filter(species==currspecies)
-  d.c<- dc[,2:3]
-  d.c <- as.data.frame(d.c)
-  d.c.sp <- SpatialPoints(d.c)
-  bb <- bbox(d.c.sp)
-  bb.buf <- extent(bb[1]-10, bb[3]+10, bb[2]-10, bb[4]+10)
-  #clim.stack <- crop(clim.stack, bb.buf)
-  bg<-randomPoints(clim.stack[[19]], n=10000)
-  bg <- as.data.frame(bg)
-  pred.mod.allyr <- ENMevaluate(d.c, clim.stack, bg, method='checkerboard2', 
-                                RMvalues=c(1,2), fc=c('L','LQ','LQP'), parallel=TRUE, algorithm='maxent.jar')
+for (h in season) try({
+  print(h)
   
-  aic.opt <- pred.mod.allyr@models[[which.max((pred.mod.allyr@results$avg.test.AUC))]]
-  dir.create(path = speciesname)
+  ses_data <- data_cl %>% 
+    dplyr::filter(season == h)
   
-  output_dir <- paste0(speciesname, "/")
+  species <- unique(ses_data$species)
   
-  ModelOutput <- 
-    as.data.frame(aic.opt@results) %>% 
-    rownames_to_column(var = "param") %>%
-    spread(key = param, value = V1) %>%
-    mutate(species = speciesname) %>%
-    dplyr::select(species, everything())
-  
-  
-  write.csv(ModelOutput, file = paste0(output_dir, "S1PalaearcticModelOutput_", speciesname, ".csv"))
-  
-  VariableContribution <- var.importance((aic.opt))
-  write.csv(VariableContribution, file = paste0(output_dir, "S1PalaearcticVariableContribution_", speciesname, ".csv"))
-  
-  r <- dismo::predict(aic.opt, clim.stack)
-  writeRaster(r, paste0(output_dir, "S1PalaearcticMap_", speciesname, ".tif"), NAflag=-9999, overwrite = TRUE)
-  
-  r_bin <- r >= ModelOutput$Maximum.training.sensitivity.plus.specificity.Cloglog.threshold
-  writeRaster(r_bin, paste0(output_dir, "S1PalaearcticMapbinary", speciesname, ".tif"), NAflag=-9999, overwrite = TRUE)
-  
-  dc_cl <- dc_cl[!(dc_cl$species %in% c(currspecies)), ]
-  
-  rm(aic.opt, bb, bb.buf, bg, d.c, d.c.sp, ModelOutput, pred.mod.allyr, r, r_bin, 
-     VariableContribution, dc, currspecies, output_dir)
-  
+  for (i in species) {
+    print(i)
+    speciesname <- gsub(" ", "_", i)
+    occs <- ses_data %>% filter(species==i)
+    occs<- occs[,2:3]
+    occs <- as.data.frame(occs)
+    
+    # Let's now remove occurrences that are cell duplicates -- these are
+    # occurrences that share a grid cell in the predictor variable rasters.
+    # Although Maxent does this by default, keep in mind that for other algorithms you may
+    # or may not want to do this based on the aims of your study.
+    # Another way to space occurrence records a defined distance from each other to avoid
+    # spatial autocorrelation is with spatial thinning (Aiello-Lammens et al. 2015).
+    occs.cells <- raster::extract(clim.stack[[1]], occs, cellnumbers = TRUE)
+    occs.cellDups <- duplicated(occs.cells[,1])
+    occs <- occs[!occs.cellDups,]
+    
+    # We'll now experiment with a different spatial R package called sf (simple features).
+    # Let's make our occs into a sf object -- as the coordinate reference system (crs) for these 
+    # points is WGS84, a geographic crs (lat/lon) and the same as our envs rasters, we specify it 
+    # as the RasterStack's crs.
+    occs.sf <- sf::st_as_sf(occs, coords = c("decimalLongitude","decimalLatitude"), crs = raster::crs(envs))
+    
+    # Now, we project our point data to an equal-area projection, which converts our 
+    # degrees to meters, which is ideal for buffering (the next step). 
+    # We use the typical Eckert IV projection.
+    eckertIV <- "+proj=eck4 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
+    occs.sf <- sf::st_transform(occs.sf, crs = eckertIV)
+    
+    # Buffer all occurrences by 500 km, union the polygons together 
+    # (for visualization), and convert back to a form that the raster package 
+    # can use. Finally, we reproject the buffers back to WGS84 (lat/lon).
+    # We choose 500 km here to avoid sampling the Caribbean islands.
+    occs.buf <- sf::st_buffer(occs.sf, dist = 100000) %>% 
+      sf::st_union() %>% 
+      sf::st_sf() %>%
+      sf::st_transform(crs = raster::crs(envs))
+    
+    # Crop environmental rasters to match the study extent
+    envs.bg <- raster::crop(envs, occs.buf)
+    # Next, mask the rasters to the shape of the buffers
+    envs.bg <- raster::mask(envs.bg, occs.buf)
+    
+    # Randomly sample 10,000 background points from one background extent raster 
+    # (only one per cell without replacement). Note: Since the raster has <10,000 pixels, 
+    # you'll get a warning and all pixels will be used for background. We will be sampling 
+    # from the biome variable because it is missing some grid cells, and we are trying to 
+    # avoid getting background points with NA. If one raster in the stack has NAs where the
+    # other rasters have data, ENMeval internally converts these cells to NA.
+    bg <- dismo::randomPoints(envs.bg[[8]], n = 10000) %>% as.data.frame()
+    colnames(bg) <- colnames(occs)
+    
+    model <- ENMevaluate(occs, envs.bg, bg, method='checkerboard2', 
+                         RMvalues=seq(0.5, 4, 0.5), fc=c("L", "LQ", "H", "LQH", "LQHP", "LQHPT"), 
+                         parallel=TRUE, kfolds = 10, algorithm='maxent.jar')
+    
+    aic.opt <- model@models[[which.max((model@results$avg.test.AUC))]]
+    dir.create(path = speciesname)
+    
+    output_dir <- paste0(speciesname, "/")
+    
+    ModelOutput <- 
+      as.data.frame(aic.opt@results) %>% 
+      rownames_to_column(var = "param") %>%
+      spread(key = param, value = V1) %>%
+      mutate(species = speciesname) %>%
+      dplyr::select(species, everything())
+    
+    
+    write.csv(ModelOutput, file = paste0(output_dir, "ModelOutput_", h, speciesname, ".csv"))
+    
+    VariableContribution <- var.importance((aic.opt))
+    write.csv(VariableContribution, file = paste0(output_dir, "VariableContribution_", h, speciesname, ".csv"))
+    
+    r <- dismo::predict(aic.opt, envs.bg)
+    writeRaster(r, paste0(output_dir, "Map_", h, speciesname, ".tif"), NAflag=-9999, overwrite = TRUE)
+    
+    r_bin <- r >= ModelOutput$Maximum.training.sensitivity.plus.specificity.Cloglog.threshold
+    writeRaster(r_bin, paste0(output_dir, "Binary_", h, speciesname, ".tif"), NAflag=-9999, overwrite = TRUE)
+    
+  }
 }, silent = FALSE)
